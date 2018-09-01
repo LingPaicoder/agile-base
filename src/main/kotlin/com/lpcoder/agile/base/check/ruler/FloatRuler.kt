ae58423597743ee300b95a453037ec8e96c503ab
LingPaicoder <15010431469@163.com>
2018-09-01 18:50:04 +0800

    FloatRuler end

---
+++ src/main/kotlin/com/lpcoder/agile/base/check/ruler/FloatRuler.kt
@@ -0,0 +1,31 @@
+package com.lpcoder.agile.base.check.ruler
+
+import com.lpcoder.agile.base.check.CheckResultCodeEnum.*
+import com.lpcoder.agile.base.check.Ruler
+import com.lpcoder.agile.base.util.NumberUtil
+
+object FloatRuler {
+    val notNull = notNull()
+    val nullVal = nullVal()
+
+    fun nullVal(code: Long = FLOAT_NULL_FAIL.code, desc: String = FLOAT_NULL_FAIL.desc)
+            = Ruler.ofNull<Float?>(code, desc)
+
+    fun notNull(code: Long = FLOAT_NOT_NULL_FAIL.code, desc: String = FLOAT_NOT_NULL_FAIL.desc)
+            = Ruler.ofNotNull<Float?>(code, desc)
+
+    fun eq(norm: Float, code: Long = FLOAT_EQ_FAIL.code, desc: String = FLOAT_EQ_FAIL.desc)
+            = Ruler.of(norm, code, desc, NumberUtil::isEq)
+
+    fun gt(norm: Float, code: Long = FLOAT_GT_FAIL.code, desc: String = FLOAT_GT_FAIL.desc)
+            = Ruler.of(norm, code, desc, NumberUtil::isGt)
+
+    fun gte(norm: Float, code: Long = FLOAT_GTE_FAIL.code, desc: String = FLOAT_GTE_FAIL.desc)
+            = Ruler.of(norm, code, desc, NumberUtil::isGte)
+
+    fun lt(norm: Float, code: Long = FLOAT_LT_FAIL.code, desc: String = FLOAT_LT_FAIL.desc)
+            = Ruler.of(norm, code, desc, NumberUtil::isLt)
+
+    fun lte(norm: Float, code: Long = FLOAT_LTE_FAIL.code, desc: String = FLOAT_LTE_FAIL.desc)
+            = Ruler.of(norm, code, desc, NumberUtil::isLte)
+}
---
+++ src/test/kotlin/com/lpcoder/agile/base/check/ruler/FloatRulerTest.kt
@@ -0,0 +1,91 @@
+package com.lpcoder.agile.base.check.ruler
+
+import com.lpcoder.agile.base.check.CheckException
+import com.lpcoder.agile.base.check.alias
+import com.lpcoder.agile.base.check.be
+import com.lpcoder.agile.base.check.must
+import com.lpcoder.agile.base.check.ruler.FloatRuler.eq
+import com.lpcoder.agile.base.check.ruler.FloatRuler.gt
+import com.lpcoder.agile.base.check.ruler.FloatRuler.gte
+import com.lpcoder.agile.base.check.ruler.FloatRuler.lt
+import com.lpcoder.agile.base.check.ruler.FloatRuler.lte
+import com.lpcoder.agile.base.check.ruler.FloatRuler.notNull
+import com.lpcoder.agile.base.check.ruler.FloatRuler.nullVal
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+
+class FloatRulerTest {
+    @get:Rule
+    val thrown: ExpectedException = ExpectedException.none()
+
+    private val weight: Float? = 1.0F
+
+
+    @Test
+    fun nullValTest() {
+        var num: Float? = null
+        num must be(nullVal)
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20000, desc=num必须为Null")
+        num = 1.0F
+        num alias "num" must be(nullVal)
+    }
+
+    @Test
+    fun notNullTest() {
+        var num: Float? = 1.0F
+        num must notNull
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20006, desc=num不能为Null")
+        num = null
+        num alias "num" must notNull
+    }
+
+    @Test
+    fun eqTest() {
+        weight must eq(1.0F)
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20001, desc=weight必须等于20")
+        weight alias "weight" must eq(20.0F)
+    }
+
+    @Test
+    fun gtTest() {
+        weight must gt(0.0F)
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20002, desc=weight必须大于20")
+        weight alias "weight" must gt(20.0F)
+    }
+
+    @Test
+    fun gteTest() {
+        weight must gte(1.0F)
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20003, desc=weight必须大于或等于20")
+        weight alias "weight" must gte(20.0F)
+    }
+
+    @Test
+    fun ltTest() {
+        weight must lt(2.0F)
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20004, desc=weight必须小于1")
+        weight alias "weight" must lt(1.0F)
+    }
+
+    @Test
+    fun lteTest() {
+        weight must lte(1.0F)
+
+        thrown.expect(CheckException::class.java)
+        thrown.expectMessage("code=-20005, desc=weight必须小于或等于0")
+        weight alias "weight" must lte(0.0F)
+    }
+}
