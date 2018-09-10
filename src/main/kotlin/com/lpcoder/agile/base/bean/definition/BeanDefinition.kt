3ca034c7dd17f89d3e0d6f942ceaf51c82103470
LingPaicoder <15010431469@163.com>
2018-09-10 08:04:56 +0800

    bean container xml parser

--- src/main/kotlin/com/lpcoder/agile/base/bean/definition/BeanDefinition.kt
+++ src/main/kotlin/com/lpcoder/agile/base/bean/definition/BeanDefinition.kt
@@ -1,12 +1,7 @@
 package com.lpcoder.agile.base.bean.definition
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class BeanDefinition(
-        @JsonProperty(value = "id")
         val id: String,
-        @JsonProperty(value = "class")
         val beanClassName: String,
-        @JsonProperty(value = "isSingleton")
         val isSingleton: Boolean
 )
