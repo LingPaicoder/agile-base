85977454cc80c83c91e10beeee4b06af87453dc5
LingPaicoder <15010431469@163.com>
2018-09-20 09:19:35 +0800

    defaultBeanContainer ok

--- src/main/kotlin/com/lpcoder/agile/base/bean/container/support/DefaultBeanContainer.kt
+++ src/main/kotlin/com/lpcoder/agile/base/bean/container/support/DefaultBeanContainer.kt
@@ -2,16 +2,62 @@ package com.lpcoder.agile.base.bean.container.support
 
 import com.lpcoder.agile.base.bean.container.BeanContainer
 import com.lpcoder.agile.base.bean.definition.BeanDefinition
+import com.lpcoder.agile.base.bean.exception.BeanCreationException
+import com.lpcoder.agile.base.bean.parser.SupportedFileTypeEnum
+import com.lpcoder.agile.base.check.alias
+import com.lpcoder.agile.base.check.must
+import com.lpcoder.agile.base.check.ruler.support.CollRuler.beNotContainsDup
 import com.lpcoder.agile.base.core.resource.Resource
+import com.lpcoder.agile.base.util.ClassUtil
+import java.util.concurrent.ConcurrentHashMap
+import java.util.stream.Collectors
 
 class DefaultBeanContainer(resource: Resource) : BeanContainer {
 
+    private val beanDefinitionMap: Map<String, BeanDefinition>
+    private val singletonObjMap: Map<String, Any>
+
+    init {
+        val beanDefinitions = SupportedFileTypeEnum
+                .getBySuffix(resource.getFileSuffix())
+                .parser.parse(resource)
+        val beanIds = beanDefinitions.stream()
+                .map(BeanDefinition::id)
+                .collect(Collectors.toList())
+        beanIds alias "beanIds" must beNotContainsDup
+
+        beanDefinitionMap = ConcurrentHashMap(64)
+        beanDefinitions.stream().forEach { beanDefinitionMap[it.id] = it }
+
+        singletonObjMap = ConcurrentHashMap(64)
+        beanDefinitions.stream()
+                .filter(BeanDefinition::isSingleton)
+                .forEach { singletonObjMap[it.id] = createBean(it) }
+    }
+
     override fun getBeanDefinition(beanId: String): BeanDefinition? {
-        return null
+        return beanDefinitionMap[beanId]
     }
 
     override fun getBean(beanId: String): Any? {
-        return null
+        val beanDefinition = beanDefinitionMap[beanId]
+        if (null == beanDefinition) {
+            return null
+        }
+        if (beanDefinition.isSingleton) {
+            return singletonObjMap[beanId]
+        }
+        return createBean(beanDefinition)
+    }
+
+    private fun createBean(beanDefinition: BeanDefinition): Any {
+        try {
+            return ClassUtil.getDefaultClassLoader()
+                    .loadClass(beanDefinition.beanClassName)
+                    .newInstance()
+        } catch (e: Exception) {
+            throw BeanCreationException("create bean for ${beanDefinition.beanClassName} failed", e)
+        }
     }
 
 }
--- src/test/kotlin/com/lpcoder/agile/base/bean/BeanContainerTest.kt
+++ src/test/kotlin/com/lpcoder/agile/base/bean/BeanContainerTest.kt
@@ -1,6 +1,7 @@
 package com.lpcoder.agile.base.bean
 
 import com.lpcoder.agile.base.bean.container.support.DefaultBeanContainer
+import com.lpcoder.agile.base.bean.parser.XMLBeanDefinitionParser
 import com.lpcoder.agile.base.bean.parser.YAMLBeanDefinitionParser
 import com.lpcoder.agile.base.bean.service.CustomService
 import com.lpcoder.agile.base.core.resource.ClassPathResource
@@ -10,19 +11,38 @@ import org.junit.Test
 
 class BeanContainerTest {
 
+    private val beanDefinitionListStr = "[" +
+            "BeanDefinition" +
+            "(id=customService, " +
+            "beanClassName=com.lpcoder.agile.base.bean.service.CustomService, " +
+            "isSingleton=true), " +
+            "BeanDefinition" +
+            "(id=invalidBean, " +
+            "beanClassName=com.lpcoder.agile.invalid.invalidBean, " +
+            "isSingleton=false)]"
+    val xmlConfigPath = "agile-bean.xml"
+    val yamlConfigPath = "agile-bean.yaml"
+    val beanId = "customService"
+    val beanClassName = "com.lpcoder.agile.base.bean.service.CustomService"
+
+
     @Test
-    fun test() {
-        println(YAMLBeanDefinitionParser()
-                .parse(ClassPathResource("agile-bean.yaml")))
+    fun testYAMLParser() {
+        assertEquals(beanDefinitionListStr, YAMLBeanDefinitionParser()
+                .parse(ClassPathResource("agile-bean.yaml"))
+                .toString())
+    }
+
+    @Test
+    fun testXMLParser() {
+        assertEquals(beanDefinitionListStr, XMLBeanDefinitionParser()
+                .parse(ClassPathResource("agile-bean.xml"))
+                .toString())
     }
 
     @Test
     fun testGetBeanByXML() {
-        val configPath = "agile-bean.xml"
-        val beanId = "customService"
-        val beanClassName = "com.lpcoder.agile.base.bean.service.CustomService"
-
-        val beanConfig = ClassPathResource(configPath)
+        val beanConfig = ClassPathResource(xmlConfigPath)
         val beanContainer = DefaultBeanContainer(beanConfig)
         val beanDefinition = beanContainer.getBeanDefinition(beanId)
         assertEquals(beanDefinition?.beanClassName, beanClassName)
@@ -33,11 +53,7 @@ class BeanContainerTest {
 
     @Test
     fun testGetBeanByYAML() {
-        val configPath = "agile-bean.yaml"
-        val beanId = "customService"
-        val beanClassName = "com.lpcoder.agile.base.bean.service.CustomService"
-
-        val beanConfig = ClassPathResource(configPath)
+        val beanConfig = ClassPathResource(yamlConfigPath)
         val beanContainer = DefaultBeanContainer(beanConfig)
         val beanDefinition = beanContainer.getBeanDefinition(beanId)
         assertEquals(beanDefinition?.beanClassName, beanClassName)
