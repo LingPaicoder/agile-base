c9a8845f51e4315b0450f838405b99a0b2d4a0dd
LingPaicoder <15010431469@163.com>
2018-09-25 09:18:49 +0800

    add test

--- src/test/kotlin/com/lpcoder/agile/base/bean/BeanContainerTest.kt
+++ src/test/kotlin/com/lpcoder/agile/base/bean/BeanContainerTest.kt
@@ -1,15 +1,21 @@
 package com.lpcoder.agile.base.bean
 
 import com.lpcoder.agile.base.bean.container.support.DefaultBeanContainer
+import com.lpcoder.agile.base.bean.exception.BeanCreationException
 import com.lpcoder.agile.base.bean.parser.XMLBeanDefinitionParser
 import com.lpcoder.agile.base.bean.parser.YAMLBeanDefinitionParser
 import com.lpcoder.agile.base.bean.service.CustomService
 import com.lpcoder.agile.base.core.resource.ClassPathResource
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 
 class BeanContainerTest {
+
+    @get:Rule
+    val thrown: ExpectedException = ExpectedException.none()
 
     private val beanDefinitionListStr = "[" +
             "BeanDefinition" +
@@ -20,23 +26,27 @@ class BeanContainerTest {
             "(id=invalidBean, " +
             "beanClassName=com.lpcoder.agile.invalid.invalidBean, " +
             "isSingleton=false)]"
-    val xmlConfigPath = "agile-bean.xml"
-    val yamlConfigPath = "agile-bean.yaml"
-    val beanId = "customService"
-    val beanClassName = "com.lpcoder.agile.base.bean.service.CustomService"
+
+    private val errXmlConfigPath = "XXX.xml"
+    private val xmlConfigPath = "agile-bean.xml"
+    private val yamlConfigPath = "agile-bean.yaml"
+
+    private val errBeanId = "invalidBean"
+    private val beanId = "customService"
+    private val beanClassName = "com.lpcoder.agile.base.bean.service.CustomService"
 
 
     @Test
     fun testYAMLParser() {
         assertEquals(beanDefinitionListStr, YAMLBeanDefinitionParser()
-                .parse(ClassPathResource("agile-bean.yaml"))
+                .parse(ClassPathResource(yamlConfigPath))
                 .toString())
     }
 
     @Test
     fun testXMLParser() {
         assertEquals(beanDefinitionListStr, XMLBeanDefinitionParser()
-                .parse(ClassPathResource("agile-bean.xml"))
+                .parse(ClassPathResource(xmlConfigPath))
                 .toString())
     }
 
@@ -61,5 +71,21 @@ class BeanContainerTest {
         val customService = beanContainer.getBean(beanId) as? CustomService
         assertNotNull(customService)
     }
+
+    @Test
+    fun testBeanCreationException() {
+        thrown.expect(BeanCreationException::class.java)
+        thrown.expectMessage("create bean for com.lpcoder.agile.invalid.invalidBean failed")
+        DefaultBeanContainer(ClassPathResource(xmlConfigPath))
+                .getBean(errBeanId) as? CustomService
+    }
+
+    /*@Test
+    fun testBeanDefinitionStoreException() {
+        //thrown.expect(BeanCreationException::class.java)
+        //thrown.expectMessage("create bean for com.lpcoder.agile.invalid.invalidBean failed")
+        DefaultBeanContainer(ClassPathResource(errXmlConfigPath))
+                .getBean(errBeanId) as? CustomService
+    }*/
 
 }
