21af8074ec06595d3978151b25058ffe5bad3d78
LingPaicoder <15010431469@163.com>
2018-11-01 09:11:58 +0800

    bean auto scan

--- src/main/kotlin/com/lpcoder/agile/base/bean/container/BeanContainer.kt
+++ src/main/kotlin/com/lpcoder/agile/base/bean/container/BeanContainer.kt
@@ -2,6 +2,7 @@ package com.lpcoder.agile.base.bean.container
 
 import com.lpcoder.agile.base.bean.container.support.definition.BeanDefinition
 
+// todo : bean生命周期的管理
 interface BeanContainer {
     fun getBean(beanId: String): Any
     fun getBeanDefinition(beanId: String): BeanDefinition
