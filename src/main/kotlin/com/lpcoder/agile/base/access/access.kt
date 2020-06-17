e11bbbf252b84538ec451361414986f4c89489ff
liurenpeng <liurenpeng@kuaishou.com>
2020-06-17 23:06:29 +0800

    add access

---
+++ src/main/kotlin/com/lpcoder/agile/base/access/access.kt
@@ -0,0 +1,61 @@
+package com.lpcoder.agile.base.access
+
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * @author liurenpeng
+ * Created on 2020-06-17
+ */
+
+interface IAccessor<K, V> {
+    fun get(keys: Collection<K>): Map<K, V>
+    fun set(dataMap: Map<K, V>) {}
+}
+
+abstract class CacheableAccessor<K, V> : IAccessor<K, V> {
+    private val cache = ConcurrentHashMap<K, V>()
+
+    override fun get(keys: Collection<K>): Map<K, V> {
+        val cached = cache.filter { keys.contains(it.key) }
+        val unCachedKeys = keys.filter { !cached.keys.contains(it) }
+        return cached + realGet(unCachedKeys)
+    }
+
+    override fun set(dataMap: Map<K, V>) {
+        cache.putAll(dataMap)
+    }
+
+    abstract fun realGet(keys: Collection<K>): Map<K, V>
+}
+
+
+fun <K, V> access(keys: Collection<K>, list: Iterable<IAccessor<K, V>>): Map<K, V> {
+    return accessByIterator(keys, list.iterator())
+}
+
+private fun <K, V> accessByIterator(keys: Collection<K>, iterator: Iterator<IAccessor<K, V>>): Map<K, V> {
+    if (!iterator.hasNext() || keys.isEmpty()) {
+        return emptyMap()
+    }
+
+    val currentAccessor = iterator.next()
+    val originalResult = currentAccessor.get(keys)
+    val leftKeys = HashSet<K>()
+    val result = mutableMapOf<K, V>()
+    for (key in keys) {
+        val value = originalResult[key]
+        if (value == null) {
+            leftKeys.add(key)
+        } else {
+            result[key] = value
+        }
+    }
+
+    val lowerResult = accessByIterator(leftKeys, iterator)
+    if (lowerResult.isNotEmpty()) {
+        currentAccessor.set(lowerResult)
+        result.putAll(lowerResult)
+    }
+    return result
+}
